4939533119eb2eb81632655876ef981e6987aba7
MAMA Anas <anas.mama@hotmail.com>
2022-01-19 16:24:47 +0100

    fonctionnalité color thief

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import React from "react";
-import Color, { Palette } from "color-thief-react";
+import ColorThief from "./components/ColorThief";
 
-const Loading = () => <div>Loading...</div>;
-export default function App() {
-  const imgSrc =
-    "http://ekladata.com/kRyR4CJUacmCnlEc-_pILyWpo0s@390x318.jpg";
+function App() {
   return (
     <div className="App">
-      <Color src={imgSrc} crossOrigin="anonymous" format="hex">
-        {({ data, loading }) => {
-          if (loading) return <Loading />;
-          return (
-            <div>
-              Predominant color: <strong>{data}</strong>
-            </div>
-          );
-        }}
-      </Color>
-      <Palette src={imgSrc} crossOrigin="anonymous" format="hex" colorCount={4}>
-        {({ data, loading }) => {
-          if (loading) return <Loading />;
-          return (
-            <div>
-              Palette:
-              <ul>
-                {data.map((color, index) => (
-                  <li key={index} style={{ color: color }}>
-                    <strong>{color}</strong>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          );
-        }}
-      </Palette>
-      <img src={imgSrc} alt="" />
+      <ColorThief/>
     </div>
   );
 }
+
+export default App;
