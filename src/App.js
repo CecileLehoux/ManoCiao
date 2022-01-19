7e96d79610db99147d33cb5975a60d1c97e63c61
MAMA Anas <anas.mama@hotmail.com>
2022-01-19 17:26:17 +0100

    fonction drag and drop

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import { useState } from "react";
-import { FileUploader } from "react-drag-drop-files";
-
 import "./App.css";
+import DragAndDrop from "./components/DragAndDrop";
 
-const fileTypes = ["JPG", "PNG", "GIF", "docx"];
-
-export default function App() {
-  const [file, setFile] = useState(null);
-  const handleChange = (file) => {
-    setFile(file);
-  };
+function App() {
   return (
     <div className="App">
-      <h1>Hello To Drag & Drop Files</h1>
-      <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
-      <p>{file ? `File name: ${file.name}` : "no files uploaded yet"}</p>
-      <p>{file}</p>
+      <DragAndDrop/>
     </div>
   );
-};
+}
+
+export default App;
---
+++ src/components/DragAndDrop.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+
+const fileTypes = ["JPG", "PNG", "GIF", "docx"];
+
+const DragAndDrop = () => {
+    const [file, setFile] = useState(null);
+  const handleChange = (file) => {
+    setFile(file);
+  };
+
+  console.log(file)
+    return (
+        <div className="App">
+          <h1>Déposez votre photo ici</h1>
+          <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+          <p>{file ? `File name: ${file.name}` : "no files uploaded yet"}</p>
+          {/* <p>{file}</p> */}
+        </div>
+      );
+}
+
+export default DragAndDrop;
