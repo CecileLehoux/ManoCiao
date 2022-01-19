035c9840ed228c1e1b6ec73c3265f52bfe8c6a7f
emma <emma.nouti@gmail.com>
2022-01-19 16:51:06 +0100

    header et premier ajout styled-component globaux

---
+++ src/component/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => (
+    <Nav>
+        <Logo>
+          <img src="assets/ManoLogo.png" alt="Logo-Mano" />
+        </Logo>
+    </Nav>
+  );
+
+const Nav = styled.nav`
+  padding-top: .5rem;
+  margin-bottom: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  background: white;
+`;
+
+const Logo = styled.a`
+  width: 50%;
+  img {
+    width: 200px;
+    padding: 10px;
+  }
+`;
+
+export default Header;
