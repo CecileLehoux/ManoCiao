e513c0918292b239a37db7cf87a83c55f6293883
emma <emma.nouti@gmail.com>
2022-01-19 14:52:12 +0100

    drag and drop file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
 
-function App() {
+import "./App.css";
+
+const fileTypes = ["JPG", "PNG", "GIF", "docx"];
+
+export default function App() {
+  const [file, setFile] = useState(null);
+  const handleChange = (file) => {
+    setFile(file);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello To Drag & Drop Files</h1>
+      <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+      <p>{file ? `File name: ${file.name}` : "no files uploaded yet"}</p>
+      <p>{file}</p>
     </div>
   );
-}
-
-export default App;
+};
