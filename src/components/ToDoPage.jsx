5e7c571e0805d0bb3d171181744dc9942c85d99e
emma <emma.nouti@gmail.com>
2022-01-20 15:34:23 +0100

    before mega fusion

--- src/components/ToDoPage.jsx
+++ src/components/ToDoPage.jsx
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
 import React from 'react';
-import {useState} from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 function ToDoPage() {
-    const [isFurnitureClicked, setIsFurnitureClicked] = useState(false)
-    console.log(isFurnitureClicked)
-    function handleFurnitureClick() {
-        setIsFurnitureClicked(!isFurnitureClicked)
-    }
+    
     return (
         <>
             <TextHolder>
                 <h1>Que souhaitez-vous faire ?</h1>
             </TextHolder>
             <ChoicesContainer>
-                    <ChoiceHolder onClick={handleFurnitureClick}>   
+                    <ChoiceHolder>   
                         <h3>#Rénovation</h3>
                         <IconHolder>
                             <img src='assets/PinceauIcone.png' alt='exterieur' width='65%' />
@@ -50,44 +44,6 @@ function ToDoPage() {
             </ChoicesContainer>
         </>
     )
-=======
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-function ToDoPage() {
-  return (
-    <>
-      <TextHolder>
-        <h1>Que souhaitez-vous faire ?</h1>
-      </TextHolder>
-      <ChoicesContainer>
-        <ChoiceHolder>
-          <h3>#Rénovation</h3>
-          <IconHolder>
-            <Link to="/ChooseImg">
-              <img src="assets/PinceauIcone.png" alt="exterieur" width="65%" />
-            </Link>
-          </IconHolder>
-        </ChoiceHolder>
-        <ChoiceHolder>
-          <h3>#Réparation</h3>
-          <IconHolder>
-            <img src="assets/MarteauIcone.png" alt="interieur" width="75%" />
-          </IconHolder>
-        </ChoiceHolder>
-        <ChoiceHolder>
-          <h3>#Staging</h3>
-          <IconHolder>
-            <Link to="/ChooseImg">
-              <img src="assets/lampeIcone.png" alt="interieur" width="75%" />
-            </Link>
-          </IconHolder>
-        </ChoiceHolder>
-      </ChoicesContainer>
-    </>
-  );
->>>>>>> dev
 }
 //<ChoiceHolder>
   //                      <h3>#Réparation</h3>
@@ -96,7 +52,6 @@ function ToDoPage() {
         //                </IconHolder>
           //          </ChoiceHolder>
 const TextHolder = styled.div`
-<<<<<<< HEAD
     display: flex;
     flex-direction: column;
     text-align: center;
@@ -123,7 +78,6 @@ const ChoiceHolder = styled.div`
     justify-content: space-around;
     width: 30%;
     h3 {
-        color: ${isFurnitureClicked ? '#1e3c87' : 'white'};
         font-weight: bold;
         font-size: large;
         text-align: center;
@@ -157,53 +111,3 @@ const IconHolder = styled.div`
 `
 
 export default ToDoPage;
-=======
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem;
-`;
-const ChoicesContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  flex-wrap: wrap;
-  background-color: #179e9f;
-  border-radius: 10px;
-  margin: 1rem;
-  padding: 1rem;
-  padding-bottom: 2rem;
-`;
-const ChoiceHolder = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-  h3 {
-    color: white;
-    font-weight: bold;
-    font-size: x-large;
-  }
-  &:hover {
-    transform: scale(1.1);
-    transition: 0.3s;
-  }
-`;
-const IconHolder = styled.div`
-  border-radius: 50%;
-  background-color: white;
-  width: 8rem;
-  height: 8rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-  img {
-    padding: 5px;
-  }
-`;
-
-export default ToDoPage;
->>>>>>> dev
